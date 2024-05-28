4330484c457f4935a054c8d1402dae4265575678
GregoryLukianov <76429464+GregoryLukianov@users.noreply.github.com>
2024-05-28 18:15:34 +0300

    dotted lines

---
+++ Assets/_Scripts/MountingLineRender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class MountingLineRender: MonoBehaviour
+    {
+        public Transform mountingPoint;
+        
+        private Transform Center => transform;
+        private LineRenderer _lineRenderer;
+
+        
+        private void Start()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+            _lineRenderer.positionCount = 2; 
+        }
+
+        private void Update()
+        {
+            _lineRenderer.SetPosition(0, Center.position);
+            _lineRenderer.SetPosition(1, mountingPoint.position);
+        }
+    }
+}
